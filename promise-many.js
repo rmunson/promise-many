670c803356d63242d684a39bc8974dc8683e3ea4
Rusell Munson <russ.munson@gmail.com>
2017-06-19 15:48:18 -0400

    comments are nice

--- promise-many.js
+++ promise-many.js
@@ -16,10 +16,25 @@
             _reject=reject;
         }
 
-        function resolveFor(promise){         
+        function resolveFor(promise){
+            // We should only need to catch errors/rejections.
+            // Values and resolves should pass-through without incident.
             return Promise.resolve(promise)[CATCH](this);
         }
-
+        /**
+         * Given a collection of promises and/or values,
+         * defer resolution and rejection until all promises
+         * in the collection have resolved or rejected.
+         *
+         * @example
+         *     promiseMany(1,2,3, Promise.resolve(4))
+         *     => resolved with [1,2,3,4]
+         *
+         *     promiseMany(1,2,Promise.reject(3), Promise.resolve(4))
+         *     => rejected with [1,2,3,4]
+         * @param  {mixed} many Array or array-like collection of promises and/or values
+         * @return {array}      Array of all resulting resolve values and reject reasons.
+         */
         return function(many){
             var finalPromise = new Promise(executor);
             var finalResolve = _resolve;
@@ -31,7 +46,7 @@
                     isRejected = true;
                     return val;
                 })
-            ).then(function(values){
+            ).then(function finalizePromise(values){
                 (isRejected ? finalReject : finalResolve)(values);
             });
             return finalPromise;
