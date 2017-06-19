6bac5fae1f47dff242eadaa6cef124fde7214ca6
Rusell Munson <russ.munson@gmail.com>
2017-06-18 21:46:35 -0400

    Delay resolve/rejection on array-like collections until all elements have been resolved or rejected.  Reject/resolve with the full set of available reasons and values.

---
+++ promise-many.js
@@ -0,0 +1,43 @@
+;(function(define){
+    'use strict'
+    define(function(require, exports, module){
+        var toArray = Array.from ? Array.from : (function(){
+            ;
+            return function(convert){
+                return slice.call(convert,0);
+            }
+        })();
+        var CATCH = "catch";
+        var _resolve;
+        var _reject;
+
+        function executor(resolve, reject){
+            _resolve=resolve;
+            _reject=reject;
+        }
+
+        function resolveFor(promise){         
+            return Promise.resolve(promise)[CATCH](this);
+        }
+
+        return function(many){
+            var finalPromise = new Promise(executor);
+            var finalResolve = _resolve;
+            var finalReject = _reject;
+            var isRejected = false;
+
+            Promise.all(
+                toArray(many).map(resolveFor, function delayRejection(val){
+                    isRejected = true;
+                    return val;
+                })
+            ).then(function(values){
+                (isRejected ? finalReject : finalResolve)(values);
+            });
+            return finalPromise;
+        }
+    });
+})(typeof module === 'object' && module.exports && typeof define !== 'function'
+    ? function (factory) { module.exports = factory(require, module.exports, module); }
+    : define
+);
